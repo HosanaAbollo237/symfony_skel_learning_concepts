2bf6e03250f672138be392241315e0425e97cbe8
HosanaAbollo237 <hosana.abollo237@gmail.com>
2020-12-18 01:21:51 +0100

    add web profiler bundle

--- skeleton/src/Controller/DefaultController.php
+++ skeleton/src/Controller/DefaultController.php
@@ -5,9 +5,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller {
 
+    /**
+     * @Route("/home", name="home")
+     */
     public function index(){
         return new Response(
             $content = "<h1>Hello World</h1>"
